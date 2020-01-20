ad1bfd2714c6c5ee21c492540609132df4f3329c
Sergey Piskunov <iloveooz@yandex.ru>
2020-01-20 14:14:17 +0700

    Update mediator.cpp

--- mediator/mediator.cpp
+++ mediator/mediator.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 class Colleague;
-class Mediator;
-class ConcreteMediator;
-class ConcreteColleague1;
-class ConcreteColleague2;
 
 class Mediator {
 public:
@@ -14,75 +11,81 @@ public:
 };
 
 class Colleague {
-protected:
-	Mediator* _mediator;
-
 public:
 	explicit Colleague(Mediator* mediator) : _mediator(mediator) {}
+protected:
+	Mediator* _mediator;
 };
 
-class ConcreteColleague1 : public Colleague {
+class ConcreteColleague : public Colleague {
 public:
-	explicit ConcreteColleague1(Mediator* mediator) : Colleague(mediator) {}
+	explicit ConcreteColleague(Mediator* mediator, const std::string &name) : Colleague(mediator) {
+		_name = name;
+	}
 
-	void Send(std::string const& message) {
+	void Send(std::string const &message) {
 		_mediator->Send(message, this);
 	}
 
-	void Notify(std::string const& message) {
-		std::cout << "Colleague1 gets message '" << message << "'" << std::endl;
+	void Notify(std::string const &message) {
+		std::cout << "Colleague " << _name.data() << " gets message '" << message << "'" << std::endl;
+		Answer();
+	}
+
+	void Answer() {
+		std::cout << "Colleague " << _name.data() << " answered Bless You!" << std::endl;
+	}
+
+	std::string& getName() {
+		return _name;
 	}
 private:
 	std::string _name;
 };
 
-class ConcreteColleague2 :public Colleague {
-public:
-	explicit ConcreteColleague2(Mediator* mediator) : Colleague(mediator) {}
-
-	void Send(std::string const& message) {
-		_mediator->Send(message, this);
-	}
-
-	void Notify(std::string const& message) {
-		std::cout << "Colleague2 gets message '" << message << "'" << std::endl;
-	}
-};
-
 class ConcreteMediator :public Mediator {
 public:
-	void SetColleague1(ConcreteColleague1* c) {
-		_m_Colleague1 = c;
-	}
-
-	void SetColleague2(ConcreteColleague2* c) {
-		_m_Colleague2 = c;
+	void AddColleague(ConcreteColleague* c) {
+		_m_Colleagues.push_back(c);
 	}
 
 	void Send(std::string const& message, Colleague* colleague) const override {
-		if (colleague == _m_Colleague1) {
-			_m_Colleague2->Notify(message);
-		}
-		else if (colleague == _m_Colleague2) {
-			_m_Colleague1->Notify(message);
-		}
+		for (ConcreteColleague* colleague : _m_Colleagues)
+			colleague->Notify(message);
 	}
+
+	void Traverse() {
+		for (ConcreteColleague* colleague : _m_Colleagues)
+			std::cout << colleague->getName().data() << std::endl;
+	}
+
 protected:
-	ConcreteColleague1* _m_Colleague1 = nullptr;
-	ConcreteColleague2* _m_Colleague2 = nullptr;
+	std::vector<ConcreteColleague*> _m_Colleagues;
 };
 
 int main() {
 	ConcreteMediator mediator;
 
-	ConcreteColleague1 c1(&mediator);
-	ConcreteColleague2 c2(&mediator);
+	std::vector <ConcreteColleague *> colleagues;
 
-	mediator.SetColleague1(&c1);
-	mediator.SetColleague2(&c2);
+	colleagues.push_back(new ConcreteColleague(&mediator, "Vasya"));
+	colleagues.push_back(new ConcreteColleague(&mediator, "Petya"));
+	colleagues.push_back(new ConcreteColleague(&mediator, "Kolya"));
+	colleagues.push_back(new ConcreteColleague(&mediator, "Tolya"));
+	colleagues.push_back(new ConcreteColleague(&mediator, "Roma"));
+	colleagues.push_back(new ConcreteColleague(&mediator, "Filya"));
+	colleagues.push_back(new ConcreteColleague(&mediator, "Alesha"));
+	colleagues.push_back(new ConcreteColleague(&mediator, "Gulya"));
+	colleagues.push_back(new ConcreteColleague(&mediator, "Sereja"));
+	colleagues.push_back(new ConcreteColleague(&mediator, "Nulya"));
 
-	c1.Send("How are you?");
-	c2.Send("Fine, thanks");
+	for (auto colleague : colleagues)
+		mediator.AddColleague(colleague);
+
+	colleagues[0]->Send("How are you?");
+	std::cout << std::endl;
+
+	mediator.Traverse();
 
 	std::cin.get();
 	return 0;
